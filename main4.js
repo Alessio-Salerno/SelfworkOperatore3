daa2e038aa1435b12974323c1327984fd0622af1
Alessio-Salerno <156314493+Alessio-Salerno@users.noreply.github.com>
2024-05-20 10:20:15 +0200

    Add files via upload

---
+++ main4.js
@@ -0,0 +1,28 @@
+let number1 = 10;
+let number2 = 20;
+
+//SOMMA
+let somma = number1 + number2 ;
+console.log(somma);
+
+// SOTTRAZIONE
+let sottrazione = number2 - number1;
+console.log(sottrazione);
+
+// MOLTIPLICAZIONE
+let moltiplicazione = number1 * number2;
+console.log(moltiplicazione);
+
+// DIVISIONE
+let divisione = number2 / number1;
+console.log(divisione);
+
+// POTENZA 1
+let potenza = number1 ** 2;
+console.log(potenza);
+
+// POTENZA 2
+let potenza2 = number2 ** 2;
+console.log(potenza2);
+
+console.log(`Con i numeri da me scelti ${number1} e ${number2} ho ottenuto vari totali, tra cui la somma che è ${somma}, la sottrazione è ${sottrazione}, la moltiplicazione è ${moltiplicazione}, la divisione è ${divisione}, la prima potenza è ${potenza} e la seconda potenza è ${potenza2}`);
